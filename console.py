0a6dcb55e531c6433d866b90398a11805c5cdbcf
CarolinaMolinaSosa <Molinasosacarolina17@gmail.com>
2023-07-09 11:24:42 -0700

    Update do_update for test

--- console.py
+++ console.py
@@ -91,30 +91,27 @@ class HBNBCommand(cmd.Cmd):
                 instances.append(str(value))
         print(instances)
 
-    def do_update(self, arg):
-        """
-        Updates an instance based on the
-        class name and id
-        by adding or updating attribute
-        """
-        args = arg.split()
-        if len(args) == 0:
+    
+    def do_update(self, argv):
+        argvs = argv.split()
+        if not argvs:
             print("** class name missing **")
-        elif args[0] not in self.all_classes:
+        elif argvs[0] not in HBNBCommand.classes.keys():
             print("** class doesn't exist **")
-        elif len(args) == 1:
+        elif len(argvs) < 2:
             print("** instance id missing **")
-        elif f"{args[0]}.{args[1]}" not in storage.all():
-            print("** no instance found **")
-        elif len(args) == 2:
-            print("** attribute name missing **")
-        elif len(args) == 3:
-            print("** value missing **")
         else:
-            name = f"{args[0]}.{args[1]}"
-            obj = storage.all()[name]
-            setattr(obj, args[2], args[3])
-            obj.save()
+            key = argvs[0] + '.' + argvs[1]
+            storage.reload()
+            if key not in storage.all():
+                print("** no instance found **")
+            elif len(argvs) < 3:
+                print("** attribute name missing **")
+            elif len(argvs) < 4:
+                print("** value missing **")
+            else:
+                setattr(storage.all()[key], argvs[2], argvs[3])
+                storage.save()
 
     def emptyline(self):
         """empty line shouldn't execute anything"""
